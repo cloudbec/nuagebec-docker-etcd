8e4e59331d0f0e468c6e2506bdabc3ba5d25d141
Michael <michael@faille.io>
2015-01-08 04:41:20 -0500

    ADD ETCD will noew Advertise client url using public IP

--- start-etcd.sh
+++ start-etcd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ETH0_IPv4=`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`
 
-/usr/local/bin/etcd  -name `hostname` -initial-advertise-peer-urls http://$ETH0_IPv4:2380  -listen-peer-urls http://$ETH0_IPv4:2380  -discovery $ETCD_DISCOVERY   -listen-client-urls http://$ETH0_IPv4:2379,http://$ETH0_IPv4:4001
+/usr/local/bin/etcd  -name `hostname` -initial-advertise-peer-urls http://$ETH0_IPv4:2380  -listen-peer-urls http://$ETH0_IPv4:2380  -discovery $ETCD_DISCOVERY   -listen-client-urls http://$ETH0_IPv4:2379,http://$ETH0_IPv4:4001 -advertise-client-urls http://$ETH0_IPv4:2379,http://$ETH0_IPv4:4001
