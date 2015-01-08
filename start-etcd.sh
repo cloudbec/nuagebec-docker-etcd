9d0aec79a8cec1749e649e7164335f2918c7a628
Michael <michael@faille.io>
2015-01-08 01:09:24 -0500

    ADD etcd public listen URL

--- start-etcd.sh
+++ start-etcd.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+ETH0_IPv4=`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`
 
-/usr/local/bin/etcd  -name `hostname` -initial-advertise-peer-urls https://`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`:2380  -listen-peer-urls https://`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`:2380  -discovery $ETCD_DISCOVERY
+/usr/local/bin/etcd  -name `hostname` -initial-advertise-peer-urls https://$ETH0_IPv4:2380  -listen-peer-urls https://$ETH0_IPv4:2380  -discovery $ETCD_DISCOVERY   -listen-client-urls http://$ETH0_IPv4:2379,http://$ETH0_IPv4:4001
