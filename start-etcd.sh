939767c1d0b54e9562b888fb5f9ed9305699141d
Michael <michael@faille.io>
2015-01-07 21:36:31 -0500

    FIX http url shemes for start-etcd.sh listen url flag

--- start-etcd.sh
+++ start-etcd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/local/bin/etcd  -name `hostname` -initial-advertise-peer-urls https://`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`:2380  -listen-peer-urls https:`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`:2380  -discovery $DISCOVERY-URL
+/usr/local/bin/etcd  -name `hostname` -initial-advertise-peer-urls https://`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`:2380  -listen-peer-urls https://`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`:2380  -discovery $DISCOVERY-URL
