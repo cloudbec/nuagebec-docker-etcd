a11d61fa672327f454178cc9d624c772851309e3
Michael <michael@faille.io>
2015-01-12 07:38:12 -0500

    Fix initial cluster

--- start-etcd.sh
+++ start-etcd.sh
@@ -4,7 +4,7 @@ ETH0_IPv4=`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`
 
 trap 'pkill etcd' TERM
 
-if etcdctl -C ETH0_IPv4:2379 member list | grep ETH0_IPv4
+if ! etcdctl -C ETH0_IPv4:2379 member list | grep ETH0_IPv4
 then
 
     /usr/local/bin/etcd  -name `hostname` -initial-advertise-peer-urls http://$ETH0_IPv4:2380  -listen-peer-urls http://$ETH0_IPv4:2380  -discovery $ETCD_DISCOVERY   -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 -advertise-client-urls http://$ETH0_IPv4:2379,http://$ETH0_IPv4:4001
