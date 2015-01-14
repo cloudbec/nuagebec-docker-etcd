16c65e59b14ea91d79f861ec2c3c456140df689c
Michael <michael@faille.io>
2015-01-14 10:14:27 -0500

    Major update. Various fix like ETCD_DISCOVERY variable detection remove docker_env-file on image (can be here on container)

--- gen-pub-discovery-URL.sh
+++ gen-pub-discovery-URL.sh
@@ -1,7 +1,13 @@
 #/bin/sh
 # If env. var. named ETCD_DISCOVERY isn't set, discover it
 
-ETCD_DISCOVERY=$(wget --quiet https://discovery.etcd.io/new?size=$1  -O - )
+#ETCD_DISCOVERY=$(wget --quiet https://discovery.etcd.io/new?size=$1  -O - )
+
+#Local etcd discuvery url using unix date
+VAR=`date +%s`
+ETCD_DISCOVERY=http://172.17.42.1:4002/v2/keys/discovery/$VAR
+curl -X PUT $ETCD_DISCOVERY/_config/size -d value=$1  #configure cluster size
+
 
 cat << EOF > docker_env_file
 ETCD_DISCOVERY=$ETCD_DISCOVERY
--- start-etcd.sh
+++ start-etcd.sh
@@ -1,37 +1,67 @@
 #!/bin/sh
 
-export ETH0_IPv4=`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`
+
+if [ -z $ETCD_DISCOVERY ]
+then
+    cat > /data/.env_etcd << EOF
+$ETCD_DISCOVERY
+EOF
+fi
+
+ETH0_IPV4=`ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | head -n1`
+export `cat docker_env_file` # Supposing we have only one variable. And, we want discovery URL
+echo "ETCD discovery URL "$ETCD_DISCOVERY
 
 trap 'pkill etcd' TERM
 
-if ! etcdctl -C $ETH0_IPv4:2379 member list | grep $ETH0_IPv4
+if  (etcdctl -C $ETH0_IPV4:2379 member list | grep $ETH0_IPV4)
 then
+        echo "this node is a cluster member"
+    export ETCD_DISCOVERY=""
 
-    /usr/local/bin/etcd  -name `hostname` -initial-advertise-peer-urls http://$ETH0_IPv4:2380  -listen-peer-urls http://$ETH0_IPv4:2380  -discovery $ETCD_DISCOVERY   -listen-client-urls http://$ETH0_IPV4:2379,http://$ETH0_IPV4:4001 -advertise-client-urls http://$ETH0_IPv4:2379,http://$ETH0_IPv4:4001
+    while read env_var; do
+        export $env_var
+    done < /data/.env_etcd
 
-else    
-    if [ -x /data/`hostname`.etcd ] 
+    echo $(etcd -name `hostname` -listen-client-urls  http://$ETH0_IPV4:2379,http://$ETH0_IPV4:4001 -advertise-client-urls http://$ETH0_IPv4:2379,http://$ETH0_IPv4:4001  -listen-peer-urls http://$ETH0_IPV4:2380 -initial-advertise-peer-urls http://$ETH0_IPV4:2380 2>&1)
+       
+else
+    /usr/local/bin/etcd  -name `hostname` -initial-advertise-peer-urls http://$ETH0_IPV4:2380  -listen-peer-urls http://$ETH0_IPV4:2380  -discovery $ETCD_DISCOVERY   -listen-client-urls http://$ETH0_IPV4:2379,http://$ETH0_IPV4:4001 -advertise-client-urls http://$ETH0_IPV4:2379,http://$ETH0_IPV4:4001  2>&1 1>/tmp/out.log &
+
+    sleep 5
+
+    if  (grep "etcd: discovery cluster full, falling back to proxy"  `find  /var/log/supervisor/ | grep etcd-stdout`)
     then
+        echo "Change proxy for member"        
 
+        sleep 3
+
+        etcdctl -C $ETH0_IPV4:2379 member add `hostname` http://$ETH0_IPV4:2380 | grep -v "Added member named" | grep -v '^$' > /data/.env_etcd
+        #  supervisorctl stop etcd
+
+
+k
         export ETCD_DISCOVERY=""
+
+        supervisorctl stop etcd
 
         while read env_var; do
             export $env_var
         done < /data/.env_etcd
-
-        etcd -name `hostname` -listen-client-urls  http://$ETH0_IPV4:2379,http://$ETH0_IPV4:4001 -advertise-client-urls http://$ETH0_IPv4:2379,http://$ETH0_IPv4:4001  -listen-peer-urls http://$ETH0_IPV4:2380 -initial-advertise-peer-urls http://$ETH0_IPV4:2380
         
+        
+        echo $(etcd -name `hostname` -listen-client-urls  http://$ETH0_IPV4:2379,http://$ETH0_IPV4:4001 -advertise-client-urls http://$ETH0_IPv4:2379,http://$ETH0_IPv4:4001  -listen-peer-urls http://$ETH0_IPV4:2380 -initial-advertise-peer-urls http://$ETH0_IPV4:2380 2>&1)
+
+        supervisorctl start etcd
+
+
     else
 
-        /usr/local/bin/etcd  -name `hostname` -initial-advertise-peer-urls http://$ETH0_IPv4:2380  -listen-peer-urls http://$ETH0_IPv4:2380  -discovery $ETCD_DISCOVERY   -listen-client-urls http://$ETH0_IPV4:2379,http://$ETH0_IPV4:4001 -advertise-client-urls http://$$ETH0_IPV4:2379,http://$$ETH0_IPV4:4001 &
-
-        sleep 10
-        
-        etcdctl -C $ETH0_IPV4:2379 member add `hostname` http://$ETH0_IPV4:2380 > /data/.env_etcd
-        supervisorctl stop etcd
-        supervisorctl start etcd
+        echo "initiate cluster"
+        wait $(pgrep etcd) #hold on... since cluster run in background
 
     fi
 fi
 
+echo "shuting down etcd"
 
